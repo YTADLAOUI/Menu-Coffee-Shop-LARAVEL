2d8ee5240f152a936890f9ea48903cf14a11d28f
youssef tadlaoui <usftadlaoui@gmail.com>
2023-02-07 14:21:34 +0100

    auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,11 +3,24 @@
 @section('content')
     <div class="cover"></div>
     <nav class= "d-flex justify-content-between ps-5 pe-3">
-        <div class="logo"><h3>coffe</h3></div>
-        <div class=menu>
+        <div class="logo d-flex align-items-center"><h3>coffe</h3></div>
+        {{-- <div class=menu>
             <a href=""><button type="button" class="btn btn-outline-success me-2">Success</button></a>
             <a href=""><button type="button" class="btn btn-outline-danger">Danger</button></a>
+        </div> --}}
+        @if (Route::has('login'))
+        <div class="px-6 py-4 me-2">
+            @auth
+                <a href="{{ url('/dashboard') }}" class="text-white">Dashboard</a>
+            @else
+                <a href="{{ route('login') }}" class="text-white me-3 log ">Log in</a>
+
+                @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="text-white reg">Register</a>
+                @endif
+            @endauth
         </div>
+    @endif
     </nav>
     <main>
         <div class="d-flex justify-content-center mt-3 text-white"><h2>Menu</h2></div>
